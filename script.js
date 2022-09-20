85105416aa28e523889a30c637ea276ecebd2613
Mike <73565523+Mike-USdeA@users.noreply.github.com>
2022-09-20 14:01:10 -0400

    Completed the password verification code.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function isStrongPassword(passWord) {
     if (passWord.length >= 8 && passWord.indexOf("password") == -1) {
-        return true;
+        for (i=0; i<passWord.length; i++) {
+            if (passWord.charCodeAt(i) >= 65 && passWord.charCodeAt(i) <= 90) {
+                return true;
+            }
+        }return false;
     }else {
         return false;
     }
  }
  
-console.log(isStrongPassword("password"));
-console.log("ddd");
  
  
 
